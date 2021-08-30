189e7c5e62f170c195bd419ebe17affbbfdfea81
zhanglei <zhanglei2@pmlabs.com.cn>
2021-08-30 18:28:04 +0800

    Type: fix, add; Subject: fix bug, add test case

--- python/practice/easy/two_sum.py
+++ python/practice/easy/two_sum.py
@@ -5,10 +5,16 @@
 # You can return the answer in any order.
 from typing import Dict, Tuple, List
 
-def two_sum( nums: List[int], target: int) -> List[int]:
+
+def two_sum(nums: List[int], target: int) -> List[int]:
     length = len(nums)
     for i in range(length):
-        for j in range(length):
+        for j in range(i + 1, length):  # i+1, otherwise will loop from first item
             if nums[i] + nums[j] == target:
                 return [i, j]
     return []
+
+# test case 1
+# [3,2,4], 6
+# test case 2
+# [3,3], 6
