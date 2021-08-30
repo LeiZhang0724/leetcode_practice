1fd6c54a467d2e6bdff311686279f75cc02f0778
zhanglei <zhanglei2@pmlabs.com.cn>
2021-08-30 18:10:25 +0800

    Type: create, add; Subject: create leetcode practice project, add twoSum practice

---
+++ python/main.py
@@ -0,0 +1,16 @@
+# coding=utf-8
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+from practice.easy import two_sum
+
+
+# Press the green button in the gutter to run the script.
+a=[2,7,11,15]
+b=9
+if __name__ == '__main__':
+    print(two_sum.two_sum(a, b))
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
